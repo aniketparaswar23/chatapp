4c3d6d879307048b09f5d3192ce52815329eb677
Aniket Paraswar <aniket.paraswar23@spit.ac.in>
2024-04-19 23:02:53 +0530

    added widgets to login/signup screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -11,6 +11,23 @@ class AuthScreen extends StatefulWidget{
 }
 
 class _AuthScreenState extends State <AuthScreen>{
+
+  final _form = GlobalKey<FormState>();
+
+  var _islogin=true;
+  var _enteredEmail='';
+  var _enteredPassword='';
+  void _submit()
+  {
+    final isValid = _form.currentState!.validate();
+    if(isValid)
+    {
+      _form.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +52,7 @@ class _AuthScreenState extends State <AuthScreen>{
                   child:Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
+                      key: _form,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -45,12 +63,48 @@ class _AuthScreenState extends State <AuthScreen>{
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            validator: (value){
+                              if(value== null||value.trim().isEmpty||!value.contains('@'))
+                              {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                            onSaved: (value){
+                              _enteredEmail=value!;
+                            },
                           ),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Password',
                             ),
                             obscureText: true,
+                            validator: (value){
+                              if(value==null || value.trim().length < 6)
+                              {
+                                return 'Password must be greater 6 chars';
+                              }
+                              return null;
+                            },
+                             onSaved: (value){
+                              _enteredPassword=value!;
+                            },
+                          ),
+                          const SizedBox(height: 12,),
+                          ElevatedButton(
+                            onPressed:_submit, 
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primaryContainer
+                            ),
+                            child:  Text(_islogin ? 'Login' : 'SignUp')
+                          ),
+                          TextButton(
+                            onPressed: (){
+                              setState(() {
+                                _islogin=!_islogin;
+                              });
+                            }, 
+                              child: Text(_islogin ? 'Create an Account':'I already have an account'),
                           )
                         ],
                         )
